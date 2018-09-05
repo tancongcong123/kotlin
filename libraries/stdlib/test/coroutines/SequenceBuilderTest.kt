e6a3cec1eb4b04b6b428cf7af126dc56872f124b
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2018-09-05 16:05:28 +0300

    Switch coroutine tests to release coroutines

--- libraries/stdlib/test/coroutines/SequenceBuilderTest.kt
+++ libraries/stdlib/test/coroutines/SequenceBuilderTest.kt
@@ -6,8 +6,8 @@
 package test.coroutines
 
 import kotlin.test.*
-import kotlin.coroutines.experimental.buildSequence
-import kotlin.coroutines.experimental.buildIterator
+//import kotlin.coroutines.experimental.buildSequence
+//import kotlin.coroutines.experimental.buildIterator
 
 class SequenceBuilderTest {
     @Test
